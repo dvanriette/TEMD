a70c1a75c2fb553a13b670c178df0e797746daf5
Naven Rosales <141290293+EnrisDaKitty@users.noreply.github.com>
2023-08-24 19:54:02 -0600

    Update leaderboard.js
    Added much more efficient code to run

--- leaderboard.js
+++ leaderboard.js
@@ -1,121 +1,79 @@
-document.addEventListener('DOMContentLoaded', function () {
-const top1stName = document.getElementById('1stPlcName');
-const top1stScore = document.getElementById('1stPlcScore');
+<html>
+    <link rel='stylesheet' href='leaderboard.css'>
+    <script src="leaderboard.js"></script>
+    <title>Project Leaderboard</title>
+    <head>
 
-const top2ndName = document.getElementById('2ndPlcName');
-const top2ndScore = document.getElementById('2ndPlcScore');
+    </head>
+    <body>
+        <h1 id='leadrHeader'>WOOOO LEADERBOARD</h1>
+        <p id='showTop'>Todays top contenders!: </p>
 
-const top3rdName = document.getElementById('3rdPlcName');
-const top3rdScore = document.getElementById('3rdPlcScore');
+        <p class='plcHolder' id='1thPlc'>1st Place</p>
+        <table id='1stleadrTable'>
+            <tr>
+                <td class='leadrName' id='1thPlcName'>1st Place Name</td>
+                <td class='score' id='1thPlcScore'>1st Place Score</td>
+            </tr>
+        </table>
 
-const top4thName = document.getElementById('4thPlcName');
-const top4thScore = document.getElementById('4thPlcScore');
+        <p class='plcHolder' id='2thPlc'>2nd Place</p>
+        <table id='2ndleadrTable'>
+            <tr>
+                <td class='leadrName' id='2thPlcName'>2nd Place Name</td>
+                <td class='score' id='2thPlcScore'>2nd Place Score</td>
+            </tr>
+        </table>
 
-const top5thName = document.getElementById('5thPlcName');
-const top5thScore = document.getElementById('5thPlcScore');
-
-const top6thName = document.getElementById('6thPlcName');
-const top6thScore = document.getElementById('6thPlcScore');
-
-const top7thName = document.getElementById('7thPlcName');
-const top7thScore = document.getElementById('7thPlcScore');
-
-const top8thName = document.getElementById('8thPlcName');
-const top8thScore = document.getElementById('8thPlcScore');
-
-const top9thName = document.getElementById('9thPlcName');
-const top9thScore = document.getElementById('9thPlcScore');
-
-const top10thName = document.getElementById('10thPlcName');
-const top10thScore = document.getElementById('10thPlcScore');
-
-
-const winnersArray = [];
+    
+        <p class='plcHolder' id='3thPlc'>3rd Place</p>
+        <table id='3rdleadrTable'>
+        <tr>
+            <td class='leadrName' id='3thPlcName'>3rd Place Name</td>
+            <td class='score' id='3thPlcScore'>3rd Place Score</td>
+        </tr>
+        </table>
 
 
-
-  var playerData = [];
-  console.log("D")
-  const date = new Date()
-  const dom = date.getDate()
-  const dateString = dom.toString()
-  console.log("a")
-  const fetchPromise = fetch("http://localhost:5200/users");
-  fetchPromise.then(response => {
-      return response.json() })
-      .then(list=>{
-          console.log("c")
-          usersForToday = []
-          usersWithoutZero = []
-          list.forEach(element => {
-            if(!(element["guessCount"] === 0)){
-                usersWithoutZero.push(element)
-            }
-        })
-          usersWithoutZero.forEach(element => {
-              if(element["dateOfLastLogin"] === dateString){
-                  usersForToday.push(element)
-              }
-          })
-          playerData = usersForToday
-          run()
-          console.log(playerData)
-        })
-function sortWinnersByGuessCount(playerData){
-    let sortedData = playerData.sort((a,b) => a.guessCount - b.guessCount);
-
-    for(i=0; i<10 && i < playerData.length; i++){
-        winnersArray.push(sortedData[i]);
-    }
-}
-
-  function readPlayers(topPlayers = []){
-    for (i = 0; i<topPlayers.length; i++){
-        console.log(winnersArray[i])
-    }
-  }
-
-  function pushWinnersToLeaderboard(winners = []){
-    try{
-          top1stName.textContent = winners[0].username;
-          top1stScore.textContent = winners[0].guessCount;
-
-          top2ndName.textContent = winners[1].username;
-          top2ndScore.textContent = winners[1].guessCount;
-          
-          top3rdName.textContent = winners[2].username;
-          top3rdScore.textContent = winners[2].guessCount;
-
-          top4thName.textContent = winners[3].username;
-          top4thScore.textContent = winners[3].guessCount;
-
-          top5thName.textContent = winners[4].username;
-          top5thScore.textContent = winners[4].guessCount;
-
-          top6thName.textContent = winners[5].username;
-          top6thScore.textContent = winners[5].guessCount;
-
-          top7thName.textContent = winners[6].username;
-          top7thScore.textContent = winners[6].guessCount;
-
-          top8thName.textContent = winners[7].username;
-          top8thScore.textContent = winners[7].guessCount;
-
-          top9thName.textContent = winners[8].username;
-          top9thScore.textContent = winners[8].guessCount;
-
-          top10thName.textContent = winners[9].username;
-          top10thScore.textContent = winners[9].guessCount;
-  }catch{
-    console.log("not enough users")
-  }
-    }
-
-  async function run(){
-    sortWinnersByGuessCount(playerData);
-    pushWinnersToLeaderboard(winnersArray);
-  }
-
-  //run();
-//readPlayers(winnersArray);
-});
+        <table id='leadrTable'>
+            <tr>
+                <td class='plcNum' value='4thPlc'>4th</td>
+                <td class='leadrName' id='4thPlcName'>4th Place Name</td>
+                <td class='score' id='4thPlcScore'>4th Place Score</td>
+            </tr>
+            <tr>
+                <td class='plcNum' value='5thPlc'>5th</td>
+                <td class='leadrName' id='5thPlcName'>5th Place Name</td>
+                <td class='score' id='5thPlcScore'>5th Place Score</td>
+            </tr>
+            <tr>
+                <td class='plcNum' value='6thPlc'>6th</td>
+                <td class='leadrName' id='6thPlcName'>6th Place Score</td>
+                <td class='score' id='6thPlcScore'>6th Place Score</td>
+            </tr>
+            <tr>
+                <td class='plcNum' value='7thPlc'>7th</td>
+                <td class='leadrName' id='7thPlcName'>7th Place Name</td>
+                <td class='score' id='7thPlcScore'>7th Place Score</td>
+            </tr>
+            <tr>
+                <td class='plcNum' value='8thPlc'>8th</td>
+                <td class='leadrName' id='8thPlcName'>8th Place Name</td>
+                <td class='score' id='8thPlcScore'>8th Place Score</td>
+            </tr>
+            <tr>
+                <td class='plcNum' value='9thPlc'>9th</td>
+                <td class='leadrName' id='9thPlcName'>9th Place Name</td>
+                <td class='score' id='9thPlcScore'>9th Place Score</td>
+            </tr>
+            <tr>
+                <td class='plcNum' value='10thPlc'>10th</td>
+                <td class='leadrName' id='10thPlcName'>10th Place Name</td>
+                <td class='score' id='10thPlcScore'>10th Place Score</td>
+            </tr>
+        </table>
+        <div id='homeBtn'>
+            <button onclick="window.location.href = 'prototype.html';">HOME</button>
+        </div>
+    </body>
+</html>
