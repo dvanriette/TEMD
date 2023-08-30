b46efb7d66530a44bedcffc45bc72d5246c87eda
wispeey <129351978+wispeey@users.noreply.github.com>
2023-08-29 19:44:15 -0600

    final stuff

--- leaderboard.js
+++ leaderboard.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
             score: document.getElementById(`${i}thPlcScore`)
         };
     }
-    
+
     // const playerData = [ //This json would be our player data. This is for testing
     //     {
     //         "username": "Example Name",
@@ -20,6 +20,45 @@ document.addEventListener('DOMContentLoaded', function () {
     //         "dateOfLastLogin": "2023-08-23"
     //     }
     // ];
+
+
+    var playerData = [];
+    console.log("D")
+    const date = new Date()
+    const dom = date.getDate()
+    const dateString = dom.toString()
+    console.log("a")
+    const fetchPromise = fetch("http://localhost:5200/users");
+    fetchPromise.then(response => {
+        return response.json()
+    })
+        .then(list => {
+            console.log("c")
+            usersForToday = []
+            usersWithoutZero = []
+            list.forEach(element => {
+                if (!(element["guessCount"] === 0)) {
+                    usersWithoutZero.push(element)
+                }
+            })
+            usersWithoutZero.forEach(element => {
+                if (element["dateOfLastLogin"] === dateString) {
+                    usersForToday.push(element)
+                }
+            })
+            avg = 0
+            usersForToday.forEach(user => {
+                avg = avg + user["guessCount"]
+            });
+            if (usersForToday.length > 0){
+                avg = avg/usersForToday.length
+            }
+            document.getElementById('showTop').innerText = "Todays top contenders! Average Score: " + avg
+
+            playerData = usersForToday
+            run()
+            console.log(playerData)
+        })
 
     function sortWinnersByGuessCount(playerData) {
         let sortedData = playerData.sort((a, b) => a.guessCount - b.guessCount);
@@ -38,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function pushWinnersToLeaderboard(winners = []) {
         for (let i = 0; i < winners.length && i < topElements.length; i++) {
             topElements[i + 1].name.textContent = winners[i].username;
-            topElements[i+ 1].score.textContent = winners[i].guessCount;
+            topElements[i + 1].score.textContent = winners[i].guessCount;
         }
     }
     async function run() {
